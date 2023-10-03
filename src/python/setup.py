63a6960b15811c2b16063cfae790fa4520cc2a02
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2023-10-03 13:07:42 +0700

    Update setup.py

--- src/python/setup.py
+++ src/python/setup.py
@@ -4,8 +4,8 @@ setup(
     name='dutwrapper',
     version='1.7.0',
     author='dutwrapper-lib',
-    author_email='ZoeMeow5466@outlook.com',
-    url='https://github.com/dutwrapper-lib/python',
+    author_email='ZoeMeow1027@outlook.com',
+    url='https://github.com/ZoeMeow1027/dutwrapper',
     description='This library provides wrapper (for this repository - crawl data from a page) for access for some features in Da Nang University of Technology student page.',
     # list folders, not files
     packages=['dutwrapper'],
