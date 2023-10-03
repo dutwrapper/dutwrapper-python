74897b3fe3bf224c6be2c32638699b4afa067e79
Quế Tùng <47247560+ZoeMeow1027@users.noreply.github.com>
2023-10-03 20:59:18 +0700

    Update setup.py
    Update requirements for install this package.

--- src/python/setup.py
+++ src/python/setup.py
@@ -1,12 +1,13 @@
-from setuptools import setup
+from setuptools import find_packages, setup
 
 setup(
     name='dutwrapper',
     version='1.7.0',
-    author='dutwrapper-lib',
+    author='ZoeMeow',
     author_email='ZoeMeow1027@outlook.com',
     url='https://github.com/ZoeMeow1027/dutwrapper',
     description='This library provides wrapper (for this repository - crawl data from a page) for access for some features in Da Nang University of Technology student page.',
     # list folders, not files
-    packages=['dutwrapper'],
+    packages=find_packages(),
+    install_requires=['beautifulsoup4', 'certifi', 'charset-normalizer', 'idna', 'lxml', 'requests', 'soupsieve', 'urllib3'],
 )
